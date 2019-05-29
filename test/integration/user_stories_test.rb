6233d5a9d4a23660028db2416b7df5e7e1b5204e
LiudaShevliuk <liuda.shevliuk@gmail.com>
2019-05-29 23:53:35 +0300

    comments

--- test/integration/user_stories_test.rb
+++ test/integration/user_stories_test.rb
@@ -12,24 +12,24 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
 # single line item corresponding to the product they added to their cart.
   test "buying a product" do
     start_order_count = Order.count
-    ruby_book = products(:ruby)
+    ruby_book = products(:ruby) # записуємо у змінну fixtures
 
-    get "/"
+    get "/"                        # заходимо на сторінку
     assert_response :success
     assert_select 'h1', "Catalog"
 
-    post '/line_items', params: { product_id: ruby_book.id }, xhr: true
+    post '/line_items', params: { product_id: ruby_book.id }, xhr: true # додаємо товар у корзину
     assert_response :success
 
-    cart = Cart.find(session[:cart_id])
+    cart = Cart.find(session[:cart_id]) # перевіряємо, чи товар є у корзині
     assert_equal 1, cart.line_items.size
     assert_equal ruby_book, cart.line_items[0].product
 
-    get "/orders/new"
+    get "/orders/new" # переходимо до оформлення замовлення
     assert_response :success
     assert_select 'legend', 'Please Enter Your Details'
 
-    perform_enqueued_jobs do
+    perform_enqueued_jobs do # вводимо дані
       post "/orders", params: {
           order: {
               name: "Dave Thomas",
@@ -39,14 +39,14 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
           }
       }
 
-      follow_redirect!
+      follow_redirect! # повертаємося до каталогу та перевіряємо, чи кошик порожній
 
       assert_response :success
       assert_select 'h1', "Catalog"
       cart = Cart.find(session[:cart_id])
       assert_equal 0, cart.line_items.size
 
-      assert_equal start_order_count + 1, Order.count
+      assert_equal start_order_count + 1, Order.count # перевіряємо, чи усе записалося у БД
       order = Order.last
 
       assert_equal "Dave Thomas", order.name
@@ -58,7 +58,7 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
       line_item = order.line_items[0]
       assert_equal ruby_book, line_item.product
 
-      mail = OrderMailer.received(orders(:one))
+      mail = OrderMailer.received(orders(:one)) # перевіряємо лист
       assert_equal ["dave@example.com"], mail.to
       assert_equal 'Kims Optical <depot@example.com>', mail[:from].value
       assert_equal "Kims Optical Order Confirmation", mail.subject
--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -6,7 +6,6 @@ class OrderMailerTest < ActionMailer::TestCase
     assert_equal "Kims Optical Order Confirmation", mail.subject
     assert_equal ["dave@example.com"], mail.to
     assert_equal ["depot@example.com"], mail.from
-    #assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
   end
 
   test "shipped" do
@@ -14,7 +13,6 @@ class OrderMailerTest < ActionMailer::TestCase
     assert_equal "Kims Optical Order Shipped", mail.subject
     assert_equal ["dave@example.com"], mail.to
     assert_equal ["depot@example.com"], mail.from
-    #asert_match /<td>1&times;<\/td>\s*<td>The Dean<\/td>/, mail.body.encoded
   end
 
 end
